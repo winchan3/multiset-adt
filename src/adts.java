f712756c2296da604d61537053ce1ed6e45a6e6e
Winston Chan <wins.chan@mail.utoronto.ca>
2023-09-18 12:22:27 -0400

    created java adts class

---
+++ src/adts.java
@@ -0,0 +1,7 @@
+public class adts {
+    //    An abstract class representing the MultiSet ADT, which supports the
+    //    add, remove, isEmpty, count, and contains operations.
+    //
+    //    This class itself does not handle how the underlying data is stored,
+    //    so it just inherits Object.__init__.
+}
